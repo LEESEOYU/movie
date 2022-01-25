1cada8330c5b60b2322e07e897efd0e85fe1c48b
LEESEOYU <roseelsy@naver.com>
2022-01-25 20:38:29 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
-import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
+import React from "react";
+import Home from './routes/Home';
+import "./App.css";
 
-class App extends React.Component {
-  state={
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async() => {
-    const{
-      data:{
-        data:{movies},
-      },
-    } =  await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({movies,isLoading: false});
-  };
-  componentDidMount(){
-    this.getMovies();
-  }
-  render(){
-    const {isLoading, movies} = this.state;
-    return (
-    <div>
-      {isLoading
-       ? 'Loading... ' 
-       : movies.map((movie) => {
-      console.log(movie);
-      return( 
-      <Movie
-      id={movie.id}
-      year={movie.year}
-      title={movie.title}
-      summary={movie.summary}
-      poster={movie.medium_cover_image}
-      />
-      );
-    })}
-    </div>
-    );
-  }  
+function App(){
+  return <Home/>;
 }
+
 export default App;
