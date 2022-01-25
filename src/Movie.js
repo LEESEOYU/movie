3d18746d89a078b403ef665601de5e95fa6a764d
LEESEOYU <roseelsy@naver.com>
2022-01-25 19:26:41 +0900

    test

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import PropsTypes from 'props-types';
 
-function Movie(){
-  return <h1></h1>;
-};
+function Movie({id, title, year, summary, poster}){
+  return (<h4>{title}</h4>);
+}
 
-Movie.propsTypes = {};
+Movie.propsTypes = { 
+  id: PropsTypes.number.isRequired,
+  year: PropsTypes.number.isRequired,
+  title: PropsTypes.string.isRequired,
+  summary: PropsTypes.string.isRequired,
+  poster: PropsTypes.string.isRequired,
+ };
+
 export default Movie;
