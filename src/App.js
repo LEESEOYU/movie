20d8ed16fcb554de8ba52cb4ba300978d81abcf2
LEESEOYU <roseelsy@naver.com>
2022-01-26 21:45:46 +0900

    Route 설정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import "./App.css";
-import {HashRouter, Route} from 'react-router-dom';
+import React from 'react';
+import './App.css';
+import {HashRouter, Routes} from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
 
-function App(){
-  return (
+function App () {
+  return(
     <HashRouter>
-     <Route path="/" exact={true} componenet={Home}/>
-     <Route path="/about" componenet={About}/>
+      <Routes path="/" exact={true} componenet={Home}/>
+      <Routes path="/about" componenet={About}/>
     </HashRouter>
-  );
-}
+  )
+};
 
 export default App;
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function About  () {
-  return <span>About this page: I bulit it because I live Movies.</span>;
+function About () {
+  return <div>About this page: I bulit it because I love movies.</div>;
 };
 
 export default About;
